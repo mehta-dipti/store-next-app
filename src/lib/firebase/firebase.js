63d9ae53917e6496ce592c13f3bce90c9c6600f2
dipti.mehta <dipti.mehta@globallogic.com>
2023-10-30 11:54:34 +0530

    Added public firebase key

--- src/lib/firebase/firebase.js
+++ src/lib/firebase/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+  apiKey: "AIzaSyBZGp7ObiFOJc0CGS_jKFgcChAVwSK8XJ4",
+  authDomain: "store-app-a4024.firebaseapp.com",
+  projectId: "store-app-a4024",
+  storageBucket: "store-app-a4024.appspot.com",
+  messagingSenderId: "872659239083",
+  appId: "1:872659239083:web:a53cd51a02b5198076b7ee",
+  measurementId: "G-YL2QP3V3C9",
 };
 
 // Initialize Firebase
